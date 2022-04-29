1a512fddeb80f75ad7ecd857aef01644cce1fdd2
Mike <vantil.mike@gmail.com>
2022-04-29 14:26:40 -0500

    Fix typo

--- Frontend/login_app/src/LoginForm.js
+++ Frontend/login_app/src/LoginForm.js
@@ -16,7 +16,7 @@ const LoginForm = (props) => {
 			<h1>Login</h1>
 			<label htmlFor="name">Name</label>
 			<input type="text" id="name" />
-			<label labelFor="name">Password</label>
+			<label htmlFor="password">Password</label>
 			<input type="password" id="password" />
 			<button type="submit" onClick={handleSubmit}>Continue</button>
 		</form>
