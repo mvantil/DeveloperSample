5e3909206818295269ba4dd33daf7f93c918902a
imohammed <imohammed@freewheel.com>
2022-12-13 21:51:50 -0600

    Filter feature

--- Frontend/login_app/src/LoginAttemptList.js
+++ Frontend/login_app/src/LoginAttemptList.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./LoginAttemptList.css";
 
-const LoginAttempt = (props) => props.attempts.map(attempt => <li key={attempt}>{attempt.login} - {attempt.date}</li>);
+const LoginAttempt = (props) => {
+	console.log(props);
+	const {attempts, filter} = props
+	return attempts.filter(p => p.login.includes(filter)).map(attempt => <li key={attempt}>{attempt.login} - {attempt.date}</li>)
+};
 
-const LoginAttemptList = (props) => (
-	<div className="Attempt-List-Main">
-	 	<p>Recent activity</p>
-	  	<input type="input" placeholder="Filter..." />
-		<ul className="Attempt-List">
-			<LoginAttempt {...props} ></LoginAttempt>
-		</ul>
-	</div>
-);
+const LoginAttemptList = (props) => {
+    const [filter, setFilter] = useState('')
+
+	return (
+		<div className="Attempt-List-Main">
+	 		<p>Recent activity</p>
+	  		<input type="input" placeholder="Filter..." onChange={(e) => { setFilter(e.target.value) }} />
+			<ul className="Attempt-List">
+				<LoginAttempt {...props} filter={filter} ></LoginAttempt>
+			</ul>
+		</div>
+	)
+};
 
 export default LoginAttemptList;
